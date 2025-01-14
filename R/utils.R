5c0e75f92db41b8e1ae6d81c5a65171780ea8241
Claudiu Forgaci <claudiuforgaci@gmail.com>
2025-01-14 21:28:09 +0100

    Add occluder constructor helper function

---
+++ R/utils.R
@@ -0,0 +1,25 @@
+#' Create a polygon representing an occluder
+#'
+#' @param center_x Center x coordinate
+#' @param center_y Center y coordinate
+#' @param length Length of the occluder
+#' @param width Width of the occluder
+#'
+#' @return object of class sfc_POLYGON
+#' @export
+#'
+#' @examples
+#' occluder <- create_occluder(0, 0, 10, 2)
+create_occluder <- function(center_x, center_y, length, width) {
+  x_min <- center_x - length / 2
+  x_max <- center_x + length / 2
+  y_min <- center_y - width / 2
+  y_max <- center_y + width / 2
+  sf::st_polygon(list(matrix(c(x_min, y_min,
+                           x_max, y_min,
+                           x_max, y_max,
+                           x_min, y_max,
+                           x_min, y_min),
+                         ncol = 2,
+                         byrow = TRUE)))
+}
